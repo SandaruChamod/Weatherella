2bfd550563963c10ee50aae001bdcc77c6931767
SandaruChamod <sandaruchamod97@gmail.com>
2023-04-13 01:20:17 +0800

    Initial Commit

---
+++ Weatherella/WeatherellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherellaApp.swift
+//  Weatherella
+//
+//  Created by user234080 on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
